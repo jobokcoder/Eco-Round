294957315fce71bf0e45e9f270d16d6f5121d71b
jobokcoder <zjaqld@naver.com>
2021-06-22 16:08:09 +0900

    Update Calculation Function

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
-const userMoney = document.querySelector('.ecoRound__form--money');
-const userDate = document.querySelector('.ecoRound__form--date');
-const userCalBtn = document.querySelector('.ecoRound__form--cal');
+const ecoTip = document.querySelector('.ecoRound__tip--text-in');
+const userMoney = document.querySelector('.ecoRound__write--money');
+const userDate = document.querySelector('.ecoRound__write--date');
+const userCalBtn = document.querySelector('.ecoRound__write--cal');
+const ecoRoundWrite = document.querySelector('.ecoRound__write');
+const ecoRoundWriteText = document.querySelector('.ecoRound__write--text');
+const ecoRoundView = document.querySelector('.ecoRound__view');
+const ecoRoundViewMoney = document.querySelector('.ecoRound__view--money');
+const ecoRoundViewDate = document.querySelector('.ecoRound__view--days');
+const ecoRoundViewRetrunBtn = document.querySelector('.ecoRound__view--return');
 
-userMoney.addEventListener('keyup', (e) => { numberWithCommas(e.target); })
+const tips = [
+    '직장인의 평균 하루 용돈은 약 16,790원입니다.',
+    '남은 날짜는 계산한 당일 포함 날짜입니다.'
+];
+
+userMoney.addEventListener('keyup', (e) => { e.target.value = numberWithCommas(e.target.value); })
 userCalBtn.addEventListener('click', () => { moneyCal(); })
-
+ecoRoundViewRetrunBtn.addEventListener('click', () => { reset(); })
 window.addEventListener('load', () => { dateSet(); });
 
+function reset(){
+    dateSet();
+    userMoney.value = '';
+    ecoRoundWrite.style.display = 'grid';
+    ecoRoundView.style.display = 'none';
+    ecoRoundWriteText.style.display = 'none';
+}
+
 function moneyCal(){
+    if(userMoney.value === ''){
+        ecoRoundWriteText.style.display = 'block';
+        return 0;
+    }
     const today = new Date();
     const today_Time = today.getTime();
     const thatDay_Time = new Date(userDate.value).getTime();
     const difference = Math.abs(thatDay_Time - today_Time);
     const days = Math.ceil(difference / (1000 * 3600 * 24)) + 1;
-    console.log(days);
+    const money = numberWithCommas(userMoney.value).replace(/,/g,'');
+    const split_money = String(Math.floor(money / days)).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    ecoRoundViewMoney.textContent = `${split_money}원`;
+    ecoRoundViewDate.textContent = `${days}일 남았습니다 !`;
+    ecoRoundWrite.style.display = 'none';
+    ecoRoundView.style.display = 'grid';
 }
 
 function dateSet(){
@@ -24,10 +54,13 @@ function dateSet(){
     const today_Date = `${yyyy}-${mm}-${dd}`;
     userDate.setAttribute("min",today_Date);
     userDate.value = today_Date;
+    ecoTip.textContent = tips[Math.floor(Math.random() * tips.length)];
 }
 
-function numberWithCommas(x) {
-	x.value = x.value.replace(/,/g,'');
-    x.value = x.value.replace(/[^0-9]/g,'');
-	x.value = x.value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+function numberWithCommas(money) {
+    let num = money;
+	num = num.replace(/,/g,'');
+    num = num.replace(/[^0-9]/g,'');
+	num = num.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return num;
 }
